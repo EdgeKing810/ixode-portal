2aeb745e84e896c82d92afe0f5b23395c309c64a
Kishan Takoordyal <kishan@konnect.dev>
2022-10-12 19:01:25 +0400

    add particles bg

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,6 +8,7 @@ import { LocalContext } from '../wrappers/LocalContext';
 
 import { Heading } from '../components/Components';
 import LoginForm from '../components/login/LoginForm';
+import ParticlesBg from 'particles-bg';
 
 export default function Login() {
   const { theme, setTheme } = useThemeStore((state) => state);
@@ -47,7 +48,7 @@ export default function Login() {
 
   return (
     <div
-      className={`w-full h-screenm lg:h-screen p-2 lg:px-56 lg:py-4 ease-in-out duration-300 overflow-none bg-gradient-to-r from-slate-700 to-slate-900`}
+      className={`w-full h-screenm lg:h-screen p-2 lg:px-56 lg:py-4 ease-in-out duration-300 overflow-none`}
       // style={{
       //   backgroundImage: `url(${PUBLIC_URL}/public/background.webp)`,
       //   backgroundAttachment: 'fixed',
@@ -55,11 +56,15 @@ export default function Login() {
       //   backgroundSize: 'cover',
       // }}
     >
+      <div className="fixed w-screen h-screen top-0 left-0 z-0">
+        <ParticlesBg type="cobweb" color="#9582F2" />
+      </div>
+
       <div
-        className={`w-full p-1 lg:p-0 h-full lg:h-full flex flex-col items-center justify-center`}
+        className={`w-full p-1 lg:p-0 h-full lg:h-full flex flex-col items-center justify-center z-10`}
       >
         <div
-          className={`w-full lg:w-1/2 flex flex-col rounded-lg items-center justify-center bg-base-200 bg-opacity-25 p-4 lg:p-16`}
+          className={`w-full lg:w-1/2 flex flex-col rounded-lg items-center justify-center bg-base-200 bg-opacity-95 p-4 lg:p-16`}
         >
           <img
             src={`${PUBLIC_URL}/public/banner_purple.webp`}
