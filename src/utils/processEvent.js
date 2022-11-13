5c45c71ee734e80e45677bbb2c4be0dcb4fd4dda
Kishan Takoordyal <kishan@konnect.dev>
2022-11-13 16:53:22 +0400

    bug fix in processEvent

--- src/utils/processEvent.js
+++ src/utils/processEvent.js
@@ -1,120 +1,127 @@
 import { fetchData } from "./data";
 
 export const processEvent = (e, profiles, projects) => {
-    const eventData = fetchData().events;
+  const eventData = fetchData().events;
 
-    let event = { ...e };
+  if (e.event_type === 'user_register') {
+    console.log(e.description);
+  }
 
-    event.timestamp = new Date(event.timestamp);
-    event.timestamp = event.timestamp.toLocaleString();
-    event.redirect = event.redirect ? event.redirect : '/';
+  let event = { ...e };
 
-    event.icons = eventData
-      .find((ed) => ed.type === event.event_type)
-      .logo.split(';');
+  event.timestamp = new Date(event.timestamp);
+  event.timestamp = event.timestamp.toLocaleString();
+  event.redirect = event.redirect ? event.redirect : '/';
 
-      let originalDescription = event.description;
-      let odUsers = originalDescription
-        .split('usr[')
-        .filter((odu) => odu.includes(']'))
-        .map((odu) => odu.split(']')[0]);
-      let odUsersFinal = odUsers.map((odu) => {
-        let user = profiles.find((p) => p.id === odu);
-        return user ? user.username : odu;
-      });
-  
-      for (let i = 0; i < odUsers.length; i++) {
-        event.description = event.description
-          .split(`usr[${odUsers[i]}]`)
-          .join(`<u.${odUsersFinal[i]}>`);
-      }
+  event.icons = eventData
+    .find((ed) => ed.type === event.event_type)
+    .logo.split(';');
 
-      let odProjects = originalDescription
-      .split('pro[')
-      .filter((odp) => odp.includes(']'))
-      .map((odp) => odp.split(']')[0]);
-    let odProjectsFinal = odProjects.map((odp) => {
-      let project = projects.find((p) => p.id === odp);
-      return project ? project.name : odp;
-    });
+  let originalDescription = event.description;
+  let odUsers = originalDescription
+    .split('usr[')
+    .filter((odu) => odu.includes(']'))
+    .map((odu) => odu.split(']')[0]);
+  let odUsersFinal = odUsers.map((odu) => {
+    let user = profiles.find((p) => p.id === odu);
+    return user ? user.username : odu;
+  });
 
-    for (let i = 0; i < odProjects.length; i++) {
-      event.description = event.description
-        .split(`pro[${odProjects[i]}]`)
-        .join(`<p.${odProjectsFinal[i]}>`);
-    }
+  for (let i = 0; i < odUsers.length; i++) {
+    event.description = event.description
+      .split(`usr[${odUsers[i]}]`)
+      .join(`<u.${odUsersFinal[i]}>`);
+  }
 
-      let odCollections = originalDescription
-      .split('col[')
-      .filter((odc) => odc.includes(']'))
-      .map((odc) => odc.split(']')[0]);
-    // let odCollectionsFinal = odCollections.map(odc => {
-    //     let collection = collections.find(c => c.id === odc);
-    //     return collection ? collection.name : odc;
-    // })
+  let odProjects = originalDescription
+    .split('pro[')
+    .filter((odp) => odp.includes(']'))
+    .map((odp) => odp.split(']')[0]);
+  let odProjectsFinal = odProjects.map((odp) => {
+    let project = projects.find((p) => p.id === odp);
+    return project ? project.name : odp;
+  });
 
-    for (let i = 0; i < odCollections.length; i++) {
-      event.description = event.description
-        .split(`col[${odCollections[i]}]`)
-        .join(`<c.${odCollections[i]}>`);
-    }
+  for (let i = 0; i < odProjects.length; i++) {
+    event.description = event.description
+      .split(`pro[${odProjects[i]}]`)
+      .join(`<p.${odProjectsFinal[i]}>`);
+  }
 
-    let textToSplit = event.description
-      .split('<')
-      .filter((x) => x.includes('>'))
-      .map((x) => x.split('>')[0]);
-    let finalDescription = [];
+  let odCollections = originalDescription
+    .split('col[')
+    .filter((odc) => odc.includes(']'))
+    .map((odc) => odc.split(']')[0]);
 
-    for (let i = 0; i < textToSplit.length; i++) {
-        let tmpIndex = textToSplit.slice(0, i+1).filter(tts => tts === textToSplit[i]).length - 1;
-      let spl = event.description.split(`<${textToSplit[i]}>`)[tmpIndex];
+  // let odCollectionsFinal = odCollections.map(odc => {
+  //     let collection = collections.find(c => c.id === odc);
+  //     return collection ? collection.name : odc;
+  // })
 
-      if (spl.includes('>')) {
-          let tmp = spl.split('>');
-        finalDescription.push({
-          kind: 'normal',
-          data: tmp[tmp.length - 1],
-        });
-      } else {
-        finalDescription.push({
-          kind: 'normal',
-          data: spl,
-        });
-      }
+  for (let i = 0; i < odCollections.length; i++) {
+    event.description = event.description
+      .split(`col[${odCollections[i]}]`)
+      .join(`<c.${odCollections[i]}>`);
+  }
 
-      let splf = textToSplit[i].split('.');
-      if (splf.length > 1) {
-        let id = splf[1];
+  let textToSplit = event.description
+    .split('<')
+    .filter((x) => x.includes('>'))
+    .map((x) => x.split('>')[0]);
+  let finalDescription = [];
 
-        finalDescription.push({
-          kind: 'special',
-          data: id,
-        });
-      } else {
-        finalDescription.push({
-          kind: 'special',
-          data: textToSplit[i],
-        });
-      }
-    }
+  for (let i = 0; i < textToSplit.length; i++) {
+    let tmpIndex =
+      textToSplit.slice(0, i + 1).filter((tts) => tts === textToSplit[i])
+        .length - 1;
+    let spl = event.description.split(`<${textToSplit[i]}>`)[tmpIndex];
 
-    if (textToSplit.length < 0) {
+    if (spl.includes('>')) {
+      let tmp = spl.split('>');
       finalDescription.push({
         kind: 'normal',
-        data: event.description,
+        data: tmp[tmp.length - 1],
       });
     } else {
-        let tmpSpl = event.description.split(
-            `<${textToSplit[textToSplit.length - 1]}>`
-          );
-
       finalDescription.push({
         kind: 'normal',
-        data: tmpSpl[tmpSpl.length - 1],
+        data: spl,
       });
     }
 
-    event.description = [...finalDescription];
+    let splf = textToSplit[i].split('.');
+    if (splf.length > 1 && splf[0].length < 2) {
+      let id = splf[1];
 
-    return event;
-  };
+      finalDescription.push({
+        kind: 'special',
+        data: id,
+      });
+    } else {
+      finalDescription.push({
+        kind: 'special',
+        data: textToSplit[i],
+      });
+    }
+  }
+
+  if (textToSplit.length < 0) {
+    finalDescription.push({
+      kind: 'normal',
+      data: event.description,
+    });
+  } else {
+    let tmpSpl = event.description.split(
+      `<${textToSplit[textToSplit.length - 1]}>`
+    );
+
+    finalDescription.push({
+      kind: 'normal',
+      data: tmpSpl[tmpSpl.length - 1],
+    });
+  }
+
+  event.description = [...finalDescription];
+
+  return event;
+};
